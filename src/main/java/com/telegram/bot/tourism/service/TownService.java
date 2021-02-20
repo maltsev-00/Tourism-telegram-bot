5c36e538b134856605718f34d015f0d2179e451f
maltsev-00 <47332063+maltsev-00@users.noreply.github.com>
2021-02-20 14:16:23 +0300

    Update TownService.java

--- src/main/java/com/telegram/bot/tourism/service/TownService.java
+++ src/main/java/com/telegram/bot/tourism/service/TownService.java
@@ -3,12 +3,14 @@ package com.telegram.bot.tourism.service;
 import com.telegram.bot.tourism.model.Town;
 import com.telegram.bot.tourism.model.dto.TownDto;
 
+import java.util.UUID;
+
 
 public interface TownService {
 
     TownDto addNewTown(TownDto newTown);
 
-    TownDto deleteTown(Long id);
+    TownDto deleteTown(UUID id);
 
     TownDto redactionTown(Town townForRedaction);
 
